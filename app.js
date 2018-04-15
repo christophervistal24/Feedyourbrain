f9492256ff0df231bbfca97b304873223fe191df
Christopher <christophervistal24@gmail.com>
2018-04-15 20:11:14 +0800

    Add Login Form (NO BACK-END)

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine','ejs');
 app.get('/index',(req,res) => res.render('index'));
 app.get('/about', (req,res) => res.render('about'));
 app.get('/post',(req,res) => res.render('post'));
-app.get('/contact', (req,res) => res.render('contact'));
+app.get('/login', (req,res) => res.render('login'));
 app.get('/',(req,res) => res.render('index'));
 
 app.listen(3000);
