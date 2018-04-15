30b1b7b279423f1aa7095c1785c0c06badd69556
Christopher <christophervistal24@gmail.com>
2018-04-16 01:16:12 +0800

    Fix some bugs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 let express = require('express');
 let app     = express();
 
+app.set('port',(process.env.PORT || 5000));
 app.use(express.static('public'));
 app.set('view engine','ejs');
 
@@ -12,4 +13,6 @@ app.get('/post',(req,res) => res.render('post'));
 app.get('/login', (req,res) => res.render('login'));
 app.get('/',(req,res) => res.render('index'));
 
-app.listen(3000);
+app.listen(app.get('port'),function(){
+    console.log('Node app is running on port ' + app.get('port'));
+});
